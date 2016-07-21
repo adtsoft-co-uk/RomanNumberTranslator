fb66a3f4b0b3fbf074360227944bdb5502a45a38
niki-yakov <niki.yakov@gmail.com>
2016-07-21 20:09:04 +0000

    Solution completed and unit test added

--- roman/RomanNumberTranslator/RomanNumberTranslatorTests/RomanNumberTranslatorToTests.cs
+++ roman/RomanNumberTranslator/RomanNumberTranslatorTests/RomanNumberTranslatorToTests.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
 
 namespace RomanNumberTranslatorTests
 {
@@ -7,10 +8,55 @@ namespace RomanNumberTranslatorTests
     public class RomanNumberTranslatorToTests
     {
         [TestMethod]
-        public void GivenIntegerTranslateToRomanNumber()
+        public void GivenIntegerTranslateToRomanNumber64()
         {
-            
+            string expected = "LXIV";
 
+            var result = RomanNumberTranslator.RomanNumberTranslator.To(64);
+
+            Assert.AreEqual(expected, result);
         }
+
+        [TestMethod]
+        public void GivenIntegerTranslateToRomanNumber226()
+        {
+            string expected = "CCXXVI";
+
+            var result = RomanNumberTranslator.RomanNumberTranslator.To(226);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GivenIntegerTranslateToRomanNumber900()
+        {
+            string expected = "CM";
+
+            var result = RomanNumberTranslator.RomanNumberTranslator.To(900);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GivenIntegerTranslateToRomanNumber998()
+        {
+            string expected = "CMLXLVIII";
+
+            var result = RomanNumberTranslator.RomanNumberTranslator.To(998);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void GivenIntegerTranslateToRomanNumber3()
+        {
+            string expected = "III";
+
+            var result = RomanNumberTranslator.RomanNumberTranslator.To(3);
+
+            Assert.AreEqual(expected, result);
+        }
+
+
     }
 }
